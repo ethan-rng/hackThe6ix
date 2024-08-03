a35b122975c0074f3d54142b631b4e507a764eea
RobertXing <rxing24@uwo.ca>
2024-08-03 12:16:42 -0400

    view dashboard button links to profile

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -14,7 +14,9 @@ export default function Home() {
           <div className="w-1/2 mt-20 ml-20">
             <h1 className="text-6xl font-bold text-start">no more dying at concerts ❌❌❌💀💀💀</h1>
             <h1 className="mt-10 text-3xl text-start">some business line like save lives and see the signs before its joever</h1>
-            <button className="mt-10 bg-black text-white text-3xl rounded-lg p-4">View Dashboard</button>
+            <a href="/profile">
+              <button className="mt-5 bg-black text-white text-3xl rounded-lg p-4" href="/profile">View Dashboard</button>
+            </a>
           </div>
           
 
