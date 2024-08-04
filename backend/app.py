9534ca2b358dab16f2141d0f6fed7bbc596d8e47
Maya <maya.lekhi1@gmail.com>
2024-08-04 07:32:22 -0400

    resolving merge

--- backend/app.py
+++ backend/app.py
@@ -38,20 +38,79 @@ if __name__ == '__main__':
 @app.route('/analyze_frame', methods=['POST'])
 def analyze_frame():
     image = request.files.get('image')
+    venue = request.get('venue')
 
-    if not image:
+    if not image or not venue:
         return jsonify({'error': 'No image file provided'}), 400
     
     try:
         result = call_crowd_recognition(image)
+
+        db = get_db()
+        collection = db['venues']
+        query = {'venueName': venue}
+
+        venue_entry = collection.find_one(query)
+        if not venue_entry:
+            return jsonify({'error': 'No venue entry for specified'}), 400
+
+        calculate_density(venue_entry.get('venueName'), result, venue_entry.get('width_m'), venue_entry.get('height_m'))
         return jsonify({'message': result}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# @app.route('/density-metrics', methods=['GET'])
-# def density_metrics():
-    
+@app.route('/average_density', methods=['POST'])
+def average_density():
+    db = get_db()
+    dash_collection = db['dashboard']
 
-@app.route('/alert', methods=['GET'])
-def alert():
-    DENSITY_THRESHOLD = 0.0008
+    average_density = {
+        'average_density': average,
+        'venue': venue,
+    }
+
+    result = dash_collection.insert_one(average_density)
+    print(result.inserted_id)
+    return result.inserted_id
+
+@app.route('/area_utilizaton', methods=['POST'])
+def area_utilizaton():
+    db = get_db()
+    dash_collection = db['dashboard']
+
+    area_utilization = {
+        'utilization': utilization,
+        'venue': venue,
+    }
+
+    result = dash_collection.insert_one(area_utilization)
+    print(result.inserted_id)
+    return result.inserted_id 
+    
+# @app.route('/test-mongo', methods=['GET'])
+# def mongo():
+#     try:
+#         # Access the database and collection
+#         db = get_db()
+#         collection = db['alerts']  # Replace with your actual collection name
+        
+#         print(collection)
+#         # Fetch some data from the collection
+#         # Example: Fetch the first document in the collection
+#         data = collection.find_one()
+#         print(data)
+        
+#         data = serialize_document(data)
+
+#         # Return the data as a JSON response
+#         if data:
+#             return jsonify(data)
+#         else:
+#             return jsonify({'message': 'No data found'}), 404
+    
+    # except Exception as e:
+    #     # Handle any errors that occur
+    #     return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- backend/metrics.py
+++ backend/metrics.py
@@ -6,6 +6,7 @@ import os
 from api_call import call_crowd_recognition
 from datetime import datetime
 from mongodb import get_db
+from collections import defaultdict
 
 def add_alert_to_db(date_time: datetime, venue: str, message: str):
     db = get_db()
@@ -19,57 +20,91 @@ def add_alert_to_db(date_time: datetime, venue: str, message: str):
 
     result = alerts_collection.insert_one(alert)
     print(result.inserted_id)
-    return result.inserted_id    
+    return result.inserted_id
     
 def calculate_density(venue, predictions, image_width, image_height):
-    DENSITY_THRESHOLD_LOW = 0
-    DENSITY_THRESHOLD_MID = 0
-    DENSITY_THRESHOLD_HIGH = 0
+    DENSITY_THRESHOLD_LOW = 3
+    DENSITY_THRESHOLD_MID = 5
+    DENSITY_THRESHOLD_HIGH = 7
 
-    densities = []
-    
-    total_area = image_width * image_height
+    density_per_square_meter = defaultdict(int)    
+    width_m = int(image_width)
+    height_m = int(image_height)
     
     for pred in predictions:
+        print(pred['confidence'])
         x = pred['x']
         y = pred['y']
         width = pred['width']
         height = pred['height']
-        
-        bbox_area = width * height
-        
-        density = bbox_area / total_area
-        
-        densities.append({
-            'detection_id': pred['detection_id'],
-            'bounding_box_area': bbox_area,
-            'density': density
-        })
-        
+                
+        # finding the meters subsection it should be in
+        x_min = int(x / 1920 * width_m)
+        x_max = int((x + width) / 1920 * width_m)
+        y_min = int(y / 1080 * height_m)
+        y_max = int((y + height) / 1080 * height_m)
+        print(f"x_min: {x_min}, x_max: {x_max}, y_min: {y_min}, y_max: {y_max}")
+
+        for i in range(x_min, x_max + 1):
+            for j in range(y_min, y_max + 1):
+                if 0 <= i < width_m and 0 <= j < height_m:
+                    density_per_square_meter[(i, j)] += 1
+
+    density_per_square_meter1 = dict(density_per_square_meter)
+    print(density_per_square_meter1)
+
+    for (x, y), count in density_per_square_meter.items():
+        density = count
+
         if density >= DENSITY_THRESHOLD_HIGH:
-            message = f"Critical Alert: Density detected in venue {venue}: {bbox_area} has reached critical threshold. Current density: {density}."
+            message = f"Critical Alert: Density detected in Venue {venue}: Zone ({x}m, {y}m) has reached critical threshold. Current density: {density}."
             add_alert_to_db(datetime.now(), venue, message)
         elif density >= DENSITY_THRESHOLD_LOW:
-            message = f"Warning: Density detected in venue {venue}: {bbox_area} is approaching level of concern. Current density: {density}."
+            message = f"Warning: Density detected in Venue {venue}: Zone ({x}m, {y}m) is approaching level of concern. Current density: {density}."
             add_alert_to_db(datetime.now(), venue, message)
         elif density >= DENSITY_THRESHOLD_MID:
-            message = f"Alert: Density detected in venue {venue}: {bbox_area} has reached level of concern. Current density: {density}."
+            message = f"Alert: Density detected in Venue {venue}: Zone ({x}m, {y}m) has reached level of concern. Current density: {density}."
             add_alert_to_db(datetime.now(), venue, message)
     
-    return densities
+    return
 
 # Example usage with the given JSON data
 predictions = [
-    {'x': 460.3125, 'y': 863.4375, 'width': 31.875, 'height': 31.875, 'confidence': 0.1225394755601883, 'class': 'people', 'class_id': 0, 'detection_id': '949606fb-057f-445d-abe9-d67b9758edf8'},
-    {'x': 930.9375, 'y': 627.1875, 'width': 24.375, 'height': 20.625, 'confidence': 0.12189931422472, 'class': 'people', 'class_id': 0, 'detection_id': 'becc679b-d691-4f80-9496-c7eb79560231'},
-    {'x': 451.875, 'y': 421.875, 'width': 30.0, 'height': 22.5, 'confidence': 0.12095370888710022, 'class': 'people', 'class_id': 0, 'detection_id': 'f888e1b6-77bf-4713-8b95-b770b18ff4ed'},
-    {'x': 217.5, 'y': 983.4375, 'width': 30.0, 'height': 31.875, 'confidence': 0.12021209299564362, 'class': 'people', 'class_id': 0, 'detection_id': 'e5cc2feb-a801-4727-bbf6-2eed4b7a9aba'},
-    {'x': 1561.875, 'y': 656.25, 'width': 37.5, 'height': 33.75, 'confidence': 0.1197568029165268, 'class': 'people', 'class_id': 0, 'detection_id': 'a406ec7e-1ff2-40b2-8b75-4ea7e7497562'}
+    {
+      "x": 1794,
+      "y": 951,
+      "width": 44,
+      "height": 40,
+      "confidence": 0.747,
+      "class": "people",
+      "class_id": 0,
+      "detection_id": "ebeb5ef6-62a0-4978-b33a-eac2174e629f"
+    },
+    {
+      "x": 1820,
+      "y": 941,
+      "width": 44,
+      "height": 40,
+      "confidence": 0.647,
+      "class": "people",
+      "class_id": 0,
+      "detection_id": "ebeb5ef6-62a0-4978-b33a-eac2174e629f"
+    },
+    {
+      "x": 1541,
+      "y": 269,
+      "width": 42,
+      "height": 38,
+      "confidence": 0.717,
+      "class": "people",
+      "class_id": 0,
+      "detection_id": "8d742df6-7023-4a8c-ac00-3bd8d7e1f668"
+    }
 ]
 
 # IN METERS
-image_width = 1920
-image_height = 1080
+image_width = 15
+image_height = 8
 
 densities = calculate_density(1, predictions, image_width, image_height)
 print(densities)
